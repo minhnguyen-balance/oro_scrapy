35da371ae881c7bf84279cecd586656bc4079ee7
root <minh@balanceinternet.com.au>
2014-04-23 10:32:35 +0700

    Update ScrapedObjAttr

---
+++ src/Balancenet/Bundle/ScrapyBundle/Model/ScrapedObjAttr.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: minhnvt1
+ * Date: 4/23/14 - 10:25 AM
+ */
+
+namespace Balancenet\Bundle\ScrapyBundle\Model;
+
+
+class ScrapedObjAttr
+{
+    const STANDARD          = 'S';
+    const STANDARD_UPDATE   = 'T';
+    const BASE              = 'B';
+    const DETAIL_PAGE_URL   = 'U';
+    const IMAGE             = 'I';
+} 
---
+++ src/Balancenet/Bundle/ScrapyBundle/Provider/ScrapedObjAttrProvider.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: minhnvt1
+ * Date: 4/23/14 - 10:22 AM
+ */
+
+namespace Balancenet\Bundle\ScrapyBundle\Provider;
+
+use Balancenet\Bundle\ScrapyBundle\Model\ScrapedObjAttr as ScrapedObjAttr;
+
+class ScrapedAttrProvider
+{
+    /**
+     * @var array
+     */
+    protected $attributes = array(
+        ScrapedObjAttr::STANDARD   => 'STANDARD',
+        ScrapedObjAttr::STANDARD_UPDATE => 'STANDARD (UPDATE)',
+        ScrapedObjAttr::BASE => 'BASE',
+        ScrapedObjAttr::DETAIL_PAGE_URL => 'DETAIL PAGE URL',
+        ScrapedObjAttr::IMAGE => 'IMAGE'
+    );
+
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+} 
